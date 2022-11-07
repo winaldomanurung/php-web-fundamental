92dc205f7e54ff42bcc65f32fa68789c6910d895
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-11-07 17:00:21 +0700

    registrasi

---
+++ 9-registration/index.php
@@ -0,0 +1,58 @@
+<?php
+require 'functions.php';
+
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari diclick
+if (isset($_POST["cari"])) {
+  $mahasiswa = cari($_POST["keyword"]);
+}
+?>
+
+<!DOCTYPE html>
+
+<head>
+  <title>Halaman Admin</title>
+</head>
+
+<body>
+  <h1>Daftar Mahasiswa</h1>
+
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
+  <br><br>
+
+  <form action="" method="POST">
+    <input type="text " name="keyword" size="40" autofocus placeholder="Masukkan keyword pencarian..." autocomplete="off">
+    <button type="submit" name="cari">Cari!</button>
+  </form>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>No.</th>
+      <th>Aksi</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+    </tr>
+
+
+    <?php $i = 1; ?>
+    <?php foreach ($mahasiswa as $row) : ?>
+      <tr>
+        <td><?= $i; ?></td>
+        <td><a href="ubah.php?id=<?= $row["id"] ?>">Ubah</a> | <a href="hapus.php?id=<?= $row["id"] ?>" onclick="return confirm('Yakin?')">Hapus</a></td>
+        <td><img src="img/<?= $row["gambar"] ?>" width="50px"></td>
+        <td><?= $row["nrp"] ?></td>
+        <td><?= $row["nama"] ?></td>
+        <td><?= $row["email"] ?></td>
+        <td><?= $row["jurusan"] ?></td>
+      </tr>
+      <?php $i++; ?>
+    <?php endforeach; ?>
+
+  </table>
+</body>
+
+</html>

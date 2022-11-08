7990b2547f483fe971bb68700a163d8d5f135730
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-11-08 11:41:20 +0700

    pdf reporting

---
+++ 16-pdf-reporting/index.php
@@ -0,0 +1,94 @@
+<?php
+
+require 'functions.php';
+
+session_start();
+
+if (!isset($_SESSION["login"])) {
+  header("Location: login.php");
+  exit;
+}
+
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari diclick
+if (isset($_POST["cari"])) {
+  $mahasiswa = cari($_POST["keyword"]);
+}
+?>
+
+<!DOCTYPE html>
+
+<head>
+  <title>Halaman Admin</title>
+
+  <style>
+    .loader {
+      width: 100px;
+      position: absolute;
+      top: 102px;
+      left: 300px;
+      z-index: -1;
+      display: none;
+    }
+
+    @media print {
+      .logout {
+        display: none;
+      }
+    }
+  </style>
+  <script src="js/jquery-3.6.1.min.js"></script>
+  <script src="js/script.js"></script>
+</head>
+
+<body>
+
+  <a href="logout.php" class="logout">Logout</a> | <a href="cetak.php" target="_blank">Cetak</a>
+
+  <h1>Daftar Mahasiswa</h1>
+
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
+  <br><br>
+
+  <form action="" method="POST">
+    <input type="text " name="keyword" size="40" autofocus placeholder="Masukkan keyword pencarian..." autocomplete="off" id="keyword">
+    <button type="submit" name="cari" id="tombol-cari">Cari!</button>
+
+    <img src="img/loader.gif" class="loader">
+  </form>
+  <br>
+
+  <div id="container">
+    <table border="1" cellpadding="10" cellspacing="0">
+      <tr>
+        <th>No.</th>
+        <th>Aksi</th>
+        <th>Gambar</th>
+        <th>NRP</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Jurusan</th>
+      </tr>
+
+
+      <?php $i = 1; ?>
+      <?php foreach ($mahasiswa as $row) : ?>
+        <tr>
+          <td><?= $i; ?></td>
+          <td><a href="ubah.php?id=<?= $row["id"] ?>">Ubah</a> | <a href="hapus.php?id=<?= $row["id"] ?>" onclick="return confirm('Yakin?')">Hapus</a></td>
+          <td><img src="img/<?= $row["gambar"] ?>" width="50px"></td>
+          <td><?= $row["nrp"] ?></td>
+          <td><?= $row["nama"] ?></td>
+          <td><?= $row["email"] ?></td>
+          <td><?= $row["jurusan"] ?></td>
+        </tr>
+        <?php $i++; ?>
+      <?php endforeach; ?>
+
+    </table>
+  </div>
+
+</body>
+
+</html>
---
+++ 16-pdf-reporting/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  // hilangkan tonmbol cari
+  $("#tombol-cari").hide();
+
+  //event ketika keyword ditulis
+  $("#keyword").on("keyup", function () {
+    // munculkan icon loading
+    $(".loader").show();
+
+    // ajax menggunakan load
+    // $("#container").load("ajax/mahasiswa.php?keyword=" + $("#keyword").val());
+
+    // ajax menggunakan $.get()
+    $.get("ajax/mahasiswa.php?keyword=" + $("#keyword").val(), function (data) {
+      $("#container").html(data);
+      $(".loader").hide();
+    });
+  });
+});
